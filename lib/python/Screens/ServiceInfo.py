72ba3a2912a520f6ff0803964a076bd80ed27fb1
Ev0-BH <Ev0_BhT@outlook.com>
2017-11-02 22:42:06 +0000

    [serviceinfo] seperate videocodec and resolution strings

--- lib/python/Screens/ServiceInfo.py
+++ lib/python/Screens/ServiceInfo.py
@@ -207,12 +207,11 @@ class ServiceInfo(Screen):
 			videocodec = "-"
 			resolution = "-"
 			if self.info:
-				videocodec =  ("MPEG2", "AVC", "MPEG1", "MPEG4-VC", "VC1", "VC1-SM", "HEVC", "N/A")[self.info.getInfo(iServiceInformation.sVideoType)]
+				videocodec =  ("MPEG2", "MPEG4 H.264", "MPEG1", "MPEG4-VC", "VC1", "VC1-SM", "HEVC H.265", "N/A")[self.info.getInfo(iServiceInformation.sVideoType)]
 				width = self.info.getInfo(iServiceInformation.sVideoWidth)
 				height = self.info.getInfo(iServiceInformation.sVideoHeight)
 				if width > 0 and height > 0:
-					resolution = videocodec + " - "
-					resolution += "%dx%d - " % (width,height)
+					resolution = "%dx%d - " % (width,height)
 					resolution += str((self.info.getInfo(iServiceInformation.sFrameRate) + 500) / 1000)
 					resolution += ("i", "p", "")[self.info.getInfo(iServiceInformation.sProgressive)]
 					aspect = self.getServiceInfoValue(iServiceInformation.sAspect)
@@ -220,19 +219,22 @@ class ServiceInfo(Screen):
 					resolution += " - "+aspect+""
 			if "%3a//" in refstr and reftype not in (1,257,4098,4114):
 				fillList = [(_("Service name"), name, TYPE_TEXT),
-					(_("Videocodec, size & format"), resolution, TYPE_TEXT),
+					(_("Videocodec"), videocodec, TYPE_TEXT),
+					(_("Resolution & format"), resolution, TYPE_TEXT),
 					(_("Service reference"), ":".join(refstr.split(":")[:9]), TYPE_TEXT),
 					(_("URL"), refstr.split(":")[10].replace("%3a", ":"), TYPE_TEXT)]
 			else:
 				if ":/" in refstr:
 					fillList = [(_("Service name"), name, TYPE_TEXT),
-						(_("Videocodec, size & format"), resolution, TYPE_TEXT),
+						(_("Videocodec"), videocodec, TYPE_TEXT),
+						(_("Resolution & format"), resolution, TYPE_TEXT),
 						(_("Service reference"), ":".join(refstr.split(":")[:9]), TYPE_TEXT),
 						(_("Filename"), refstr.split(":")[10], TYPE_TEXT)]
 				else:
 					fillList = [(_("Service name"), name, TYPE_TEXT),
 						(_("Provider"), self.getServiceInfoValue(iServiceInformation.sProvider), TYPE_TEXT),
-						(_("Videocodec, size & format"), resolution, TYPE_TEXT)]
+						(_("Videocodec"), videocodec, TYPE_TEXT),
+						(_("Resolution & format"), resolution, TYPE_TEXT)]
 					if "%3a//" in refstr:
 						fillList = fillList + [(_("Service reference"), ":".join(refstr.split(":")[:9]), TYPE_TEXT),
 							(_("URL"), refstr.split(":")[10].replace("%3a", ":"), TYPE_TEXT)]
