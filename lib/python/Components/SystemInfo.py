00549ceaeb36daeb9304d1a84945163ddce97f12
Ev0-BH <Ev0_BhT@outlook.com>
2018-11-13 19:22:42 +0000

    Add Duo4K support 1st Commit

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -105,7 +105,7 @@ SystemInfo["yellow_RCA_no_scart"] = getHaveSCART() in ('False',) and (getHaveRCA
 # Machines that have neither yellow RCA nor Scart sockets
 SystemInfo["no_yellow_RCA__no_scart"] = getHaveRCA() in ('False',) and (getHaveSCART() in ('False',) and getHaveAVJACK() in ('False',))
 SystemInfo["VideoModes"] = getChipSetString() in ( # 2160p and 1080p capable hardware
-		'5272s', '7251', '7251s', '7252', '7252s', '7366', '7376', '7444s', '72604'
+		'5272s', '7251', '7251s', '7252', '7252s', '72604', '7278', '7366', '7376', '7444s'
 	) and (
 		["720p", "1080p", "2160p", "2160p30", "1080i", "576p", "576i", "480p", "480i"], # normal modes
 		{"720p", "1080p", "2160p", "2160p30", "1080i"} # widescreen modes
