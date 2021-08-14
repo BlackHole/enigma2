0cc39735b259e3d29fd99f109548ec3240d13fa7
Ev0-BH <Ev0_BhT@outlook.com>
2021-08-14 22:56:54 +0100

    [ServiceInfo] Remove IsIPStream (same as IsStream), fix VideoInfo to show either p or i (instead of just showing i when 50fps content is displayed) and change Hz label to fps. Swap IsSD and IsHD around so the icon defaults to HD, instead of SD before it is polled. Add more AC3 labels.

--- lib/python/Components/Converter/ServiceInfo.py
+++ lib/python/Components/Converter/ServiceInfo.py
@@ -48,14 +48,13 @@ class ServiceInfo(Poll, Converter):
 	VIDEO_INFO = 34
 	IS_SD_AND_WIDESCREEN = 35
 	IS_SD_AND_NOT_WIDESCREEN = 36
-	IS_IPSTREAM = 37
-	IS_SDR = 38
-	IS_HDR = 39
-	IS_HDR10 = 40
-	IS_HLG = 41
-	IS_VIDEO_MPEG2 = 42
-	IS_VIDEO_AVC = 43
-	IS_VIDEO_HEVC = 44
+	IS_SDR = 37
+	IS_HDR = 38
+	IS_HDR10 = 39
+	IS_HLG = 40
+	IS_VIDEO_MPEG2 = 41
+	IS_VIDEO_AVC = 42
+	IS_VIDEO_HEVC = 43
 
 	def __init__(self, type):
 		Poll.__init__(self)
@@ -99,7 +98,6 @@ class ServiceInfo(Poll, Converter):
 			"Is576": (self.IS_576, (iPlayableService.evVideoSizeChanged, iPlayableService.evUpdatedInfo, iPlayableService.evStart)),
 			"Is480": (self.IS_480, (iPlayableService.evVideoSizeChanged, iPlayableService.evUpdatedInfo, iPlayableService.evStart)),
 			"Is4K": (self.IS_4K, (iPlayableService.evVideoSizeChanged, iPlayableService.evUpdatedInfo, iPlayableService.evStart)),
-			"IsIPStream": (self.IS_IPSTREAM, (iPlayableService.evUpdatedInfo,)),
 			"IsSDR": (self.IS_SDR, (iPlayableService.evVideoGammaChanged, iPlayableService.evUpdatedInfo, iPlayableService.evStart)),
 			"IsHDR": (self.IS_HDR, (iPlayableService.evVideoGammaChanged, iPlayableService.evUpdatedInfo, iPlayableService.evStart)),
 			"IsHDR10": (self.IS_HDR10, (iPlayableService.evVideoGammaChanged, iPlayableService.evUpdatedInfo, iPlayableService.evStart)),
@@ -166,7 +164,7 @@ class ServiceInfo(Poll, Converter):
 	def _getProgressive(self, info):
 		return self._getValInt("/proc/stb/vmpeg/0/progressive", info, iServiceInformation.sProgressive, default=0)
 
-	def _getProgressiveStr(self, info, convert=lambda x: "" if x else "i"):
+	def _getProgressiveStr(self, info, convert=lambda x: "p" if x else "i"):
 		return self._getValStr("/proc/stb/vmpeg/0/progressive", info, iServiceInformation.sProgressive, convert=convert)
 
 	@cached
@@ -192,7 +190,7 @@ class ServiceInfo(Poll, Converter):
 				while idx < n:
 					i = audio.getTrackInfo(idx)
 					description = i.getDescription()
-					if description in ("AC3", "AC-3", "DTS"):
+					if description in ("AC3", "AC-3", "AC-3 (ATSC A/52)", "A_AC3", "A_AC-3", "A_AC-3 (ATSC A/52)", "A-AC-3 audio", "E-AC-3 audio", "A_EAC3", "DTS"):
 						if self.type == self.IS_MULTICHANNEL:
 							return True
 						elif self.type == self.IS_STEREO:
@@ -227,10 +225,10 @@ class ServiceInfo(Poll, Converter):
 				return video_aspect in WIDESCREEN
 			elif self.type == self.IS_NOT_WIDESCREEN:
 				return video_aspect not in WIDESCREEN
-			elif self.type == self.IS_SD:
-				return video_width > 1 and video_width <= 1024 and video_height > 1 and video_height <= 578
 			elif self.type == self.IS_HD:
 				return video_width > 1025 and video_width <= 1920 and video_height >= 481 and video_height < 1440 or video_width >= 960 and video_height == 720
+			elif self.type == self.IS_SD:
+				return video_width > 1 and video_width <= 1024 and video_height > 1 and video_height <= 578
 			elif self.type == self.IS_SD_AND_WIDESCREEN:
 				return video_height < 578 and video_aspect in WIDESCREEN
 			elif self.type == self.IS_SD_AND_NOT_WIDESCREEN:
@@ -247,8 +245,6 @@ class ServiceInfo(Poll, Converter):
 				return video_height >= 1460
 			elif self.type == self.PROGRESSIVE:
 				return bool(self._getProgressive(info))
-			elif self.type == self.IS_IPSTREAM:
-				return service.streamed() is not None
 			elif self.type == self.IS_SDR:
 				return info.getInfo(iServiceInformation.sGamma) == 0
 			elif self.type == self.IS_HDR:
@@ -328,12 +324,7 @@ class ServiceInfo(Poll, Converter):
 		elif self.type == self.VIDEO_INFO:
 			progressive = self._getProgressiveStr(info)
 			fieldrate = self._getFrameRate(info)
-			if fieldrate > 0:
-				if progressive == 'i':
-					fieldrate *= 2
-				fieldrate = "%dHz" % ((fieldrate + 500) // 1000,)
-			else:
-				fieldrate = ""
+			fieldrate = "%dfps" % ((fieldrate + 500) // 1000,)
 			return "%sx%s%s %s" % (self._getVideoWidthStr(info), self._getVideoHeightStr(info), progressive, fieldrate)
 		return ""
 
