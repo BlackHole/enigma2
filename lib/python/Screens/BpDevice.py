117ba268a917143f35206494511a791bfe5ce190
meo <lupomeo@hotmail.com>
2022-01-12 17:40:30 +0100

    BpDevice: accept microsd for PULSe machines

--- lib/python/Screens/BpDevice.py
+++ lib/python/Screens/BpDevice.py
@@ -9,6 +9,7 @@ from Components.Label import Label
 from Components.Pixmap import Pixmap
 from Components.ConfigList import ConfigListScreen
 from Components.config import getConfigListEntry, ConfigSelection, NoSave
+from boxbranding import getMachineName
 from Tools.LoadPixmap import LoadPixmap
 from Tools.Directories import fileExists, pathExists, createDir, resolveFilename, SCOPE_CURRENT_SKIN
 from os import system, listdir, remove as os_remove, rename as os_rename, stat as os_stat
@@ -38,7 +39,7 @@ class DeliteDevicesPanel(Screen):
 		self.gO()
 
 	def gO(self):
-		paths = ["/media/hdd", "/media/usb", "/media/downloads", "/media/music", "/media/personal", "/media/photo", "/media/video"]
+		paths = ["/media/hdd", "/media/usb", "/media/mmc", "/media/downloads", "/media/music", "/media/personal", "/media/photo", "/media/video"]
 		for path in paths:
 			if not pathExists(path):
 				createDir(path)
@@ -50,13 +51,18 @@ class DeliteDevicesPanel(Screen):
 		self.list = []
 		self.conflist = []
 		rc = system("blkid > /tmp/blkid.log")
-
 		f = open("/tmp/blkid.log", 'r')
 		for line in f.readlines():
-			if line.find('/dev/sd') == -1:
-				continue
+			if getMachineName().find('PULSe') != -1:
+				if line.find('/dev/sd') == -1 and line.find('mmcblk1p1') == -1:
+					continue
+			else:
+				if line.find('/dev/sd') == -1:
+					continue
 			parts = line.strip().split()
 			device = parts[0][5:-2]
+			if device == "mmcblk1p":
+				device = "mmcblk1"
 			partition = parts[0][5:-1]
 			pos = line.find("UUID") + 6
 			end = line.find('"', pos)
@@ -118,8 +124,9 @@ class DeliteDevicesPanel(Screen):
 		if fileExists(filename):
 			if open(filename).read().strip() == "0":
 				name = "HARD DISK"
-				pix = pixpath + "icons/dev_hdd.png"
-
+				pix = pixpath + "icons/dev_hdd.png"		
+		if device == "mmcblk1":
+			name = "MicroSD"
 		return name, pix
 
 	def mapSetup(self):
