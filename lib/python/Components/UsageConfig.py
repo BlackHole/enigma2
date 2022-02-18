806bc756973f362bb27a6ec9449baa4b359e272f
dpeddi <1086815+dpeddi@users.noreply.github.com>
2022-02-18 13:26:09 +0000

    disable second infobar

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -100,7 +100,7 @@ def InitUsageConfig():
 	choicelist = [("none", _("None")), ("0", _("No timeout"))] + \
 		[(str(i), ngettext("%d second", "%d seconds", i) % i) for i in [3, 5, 7, 10, 15, 20, 30, 60]] + \
 		[("EPG", _("EPG")), ("INFOBAREPG", _("InfoBar EPG"))]
-	config.usage.show_second_infobar = ConfigSelection(default="5", choices=choicelist)
+	config.usage.show_second_infobar = ConfigSelection(default="none", choices=choicelist)
 	config.usage.fix_second_infobar = ConfigYesNo(default=False)
 
 	def showsecondinfobarChanged(configElement):
