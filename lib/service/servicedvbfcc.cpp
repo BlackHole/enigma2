b2ed25e12937e86c52f3fae42f4bb799347797cb
Huevos <rowland@moralet.com>
2022-04-12 00:48:57 +0200

    Revert "[servicedvbfcc] add debug for testing"
    This reverts commit 42e395e7509339434c9e1613463693312051c675.

--- lib/service/servicedvbfcc.cpp
+++ lib/service/servicedvbfcc.cpp
@@ -138,11 +138,9 @@ void eDVBServiceFCCPlay::pushbackFCCEvents(int event)
 
 void eDVBServiceFCCPlay::popFCCEvents()
 {
-	eDebug("[eDVBServiceFCCPlay] popFCCEvents");
 	m_fcc_events.unique(); // remove duplicate evUpdatedInfo
 	for (std::list<int>::iterator it = m_fcc_events.begin(); it != m_fcc_events.end(); ++it)
 	{
-		eDebug("[eDVBServiceFCCPlay] popFCCEvents loop 1");
 		if (*it == evUpdatedInfo)
 		{
 			updateFCCDecoder();
@@ -156,10 +154,8 @@ void eDVBServiceFCCPlay::popFCCEvents()
 	/* send events */
 	for (std::list<int>::iterator it = m_fcc_events.begin(); it != m_fcc_events.end(); ++it)
 	{
-		eDebug("[eDVBServiceFCCPlay] popFCCEvents loop 2");
 		int event = *it;
 		m_event((iPlayableService*)this, event);
-		eDebug("[eDVBServiceFCCPlay] m_event: %d", event);
 	}
 }
 
