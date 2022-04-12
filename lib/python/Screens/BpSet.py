f82fe6c1d2641ebfa3aa1f4421d90617b0c24cad
Ev0-BH <Ev0_BhT@outlook.com>
2022-04-12 12:29:15 +0100

    Update speedup panel

--- lib/python/Screens/BpSet.py
+++ lib/python/Screens/BpSet.py
@@ -975,7 +975,7 @@ class BhSpeedUp(Screen, ConfigListScreen):
 		if getBoxType() not in ("vusolo", "vuduo", "vuultimo", "vuuno", "vuzero", "osninoplus", "osninopro", "zgemmah82h"):
 			self.pluglist.append(["Kodi", "enigma2-plugin-extensions-kodi"])
 
-		if getBoxType() == "sf8008" or getBoxType() == "sx988" or getBoxType() == "pulse4k" or getBoxType() == "pulse4kmini" or getBoxType() == "zgemmah11s":
+		if getBoxType() == "sf8008" or getBoxType() == "sx988" or getBoxType() == "pulse4k" or getBoxType() == "pulse4kmini" or getBoxType() == "zgemmah11s" or getBoxType() == "viper4kv40":
 			self.pluglist.append (["Chromium2 (Netflix, YoutubeTV, Disney +)", "enigma2-plugin-extensions-chromium2"])
 
 		self.activityTimer = eTimer()
