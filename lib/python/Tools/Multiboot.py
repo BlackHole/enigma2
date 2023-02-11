15e8a2671cb7312e96efa0f23e4198023e839b93
Ev0-BH <Ev0_BhT@outlook.com>
2023-02-11 14:52:29 +0000

    [Multiboot] remove spaces after USB

--- lib/python/Tools/Multiboot.py
+++ lib/python/Tools/Multiboot.py
@@ -58,7 +58,7 @@ def getMultibootslots():
 						line = open(file).read().replace("'", "").replace('"', "").replace("\n", " ").replace("ubi.mtd", "mtd").replace("bootargs=", "")
 #						print("[Multiboot][getMultibootslots]6 readlines = %s " % line)
 						slot = dict([(x.split("=", 1)[0].strip(), x.split("=", 1)[1].strip()) for x in line.strip().split(" ") if "=" in x])
-						slot["slotType"] = "eMMC" if "mmc" in slot["root"] else "USB   "
+						slot["slotType"] = "eMMC" if "mmc" in slot["root"] else " USB"
 						if fileHas("/proc/cmdline", "kexec=1") and int(slotnumber) > 3:
 							SystemInfo["HasKexecUSB"] = True
 						print("[Multiboot][getMultibootslots]6a slot", slot)
