3a34fe2fa21de9f73ba4fd44fc2f2d0dbac58728
Ev0-BH <Ev0_BhT@outlook.com>
2023-02-24 12:25:59 +0000

    [Multiboot] Remove /dev/mmcblk0p7 as not required

--- lib/python/Tools/Multiboot.py
+++ lib/python/Tools/Multiboot.py
@@ -10,7 +10,7 @@ from Components.Console import Console
 from Components.SystemInfo import SystemInfo, BoxInfo as BoxInfoRunningInstance, BoxInformation
 from Tools.Directories import fileHas, fileExists
 
-MbootList1 = ("/dev/mmcblk0p1", "/dev/mmcblk1p1", "/dev/mmcblk0p3", "/dev/mmcblk0p4", "/dev/mmcblk0p7", "/dev/mtdblock2", "/dev/block/by-name/bootoptions")
+MbootList1 = ("/dev/mmcblk0p1", "/dev/mmcblk1p1", "/dev/mmcblk0p3", "/dev/mmcblk0p4", "/dev/mtdblock2", "/dev/block/by-name/bootoptions")
 MbootList2 = ("/dev/%s" % getMachineMtdRoot(), )	# kexec kernel Vu+ multiboot
 
 class tmp:
