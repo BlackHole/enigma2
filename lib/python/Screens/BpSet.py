d54eee1d5dde95d128510df5ffa6b0961d068d27
Ev0-BH <Ev0_BhT@outlook.com>
2023-12-27 18:08:08 +0000

    [BpSet] Add HbbTv for Novaler4KPro

--- lib/python/Screens/BpSet.py
+++ lib/python/Screens/BpSet.py
@@ -973,13 +973,13 @@ class BhSpeedUp(Screen, ConfigListScreen):
 		elif getBrandOEM() == "vuplus" and getBoxType() != "vusolo":
 			self.pluglist.append(["ChromiumOS", "enigma2-plugin-extensions-chromium"])
 			self.pluglist.append(["HbbTV", "enigma2-plugin-extensions-webkithbbtv"])
-		elif getBrandOEM() != "vuplus" and getBrandOEM() != "abcom" and getBrandOEM() != "airdigital" and getBoxType() != "osninoplus" and getBoxType() != "osninopro" and getBoxType() != "gbquad4k" and getBoxType() != "gbue4k" and getBoxType() != "sfx6008" and getBoxType() != "sfx6018":
+		elif getBrandOEM() != "vuplus" and getBrandOEM() != "abcom" and getBrandOEM() != "airdigital" and getBrandOEM() != "maxytec" and getBoxType() != "osninoplus" and getBoxType() != "osninopro" and getBoxType() != "gbquad4k" and getBoxType() != "gbue4k" and getBoxType() != "sfx6008" and getBoxType() != "sfx6018":
 			self.pluglist.append(["HbbTV", "enigma2-plugin-extensions-hbbtv-webkit"])
 		elif getBrandOEM() == "abcom" or getBoxType() == "zgemmah9twinse" or getBoxType() == "zgemmah11s":
 			self.pluglist.append(["HbbTV", "enigma2-plugin-extensions-openhbbtvbrowser"])
 		elif getBrandOEM() == "gigablue" and getBoxType() == "gbquad4k" or getBoxType() == "gbue4k":
 			self.pluglist.append(["HbbTV", "enigma2-plugin-extensions-hbbtv-gb"])
-		elif getBoxType() == "zgemmah7":
+		elif getBoxType() == "zgemmah7" or getBoxType() == "novaler4kpro":
 			self.pluglist.append(["HbbTV", "enigma2-plugin-extensions-hbbtv-qt"])
 
 		if getBoxType() not in ("vusolo", "vuduo", "vuultimo", "vuuno", "vuzero", "osninoplus", "osninopro", "zgemmah82h", "sfx6008", "sfx6018"):
