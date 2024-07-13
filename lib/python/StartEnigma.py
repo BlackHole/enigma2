0b8b5b1b98feb8bf423f4481dd4a306013f9f3ea
Ev0-BH <Ev0_BhT@outlook.com>
2024-07-13 21:28:40 +0100

    [StartEnigma] Change to ntp as default time setting

--- lib/python/StartEnigma.py
+++ lib/python/StartEnigma.py
@@ -511,7 +511,7 @@ config.misc.blackradiopic = ConfigText(default=resolveFilename(SCOPE_CURRENT_SKI
 config.misc.isNextRecordTimerAfterEventActionAuto = ConfigYesNo(default=False)
 config.misc.isNextPowerTimerAfterEventActionAuto = ConfigYesNo(default=False)
 config.misc.pluginWakeupName = ConfigText(default="")
-config.misc.SyncTimeUsing = ConfigSelection(default="dvb", choices=[("dvb", _("Transponder Time")), ("ntp", _("NTP"))])
+config.misc.SyncTimeUsing = ConfigSelection(default="ntp", choices=[("dvb", _("Transponder Time")), ("ntp", _("NTP"))])
 config.misc.NTPserver = ConfigText(default='pool.ntp.org', fixed_size=False)
 config.misc.useNTPminutes = ConfigSelection(default="30", choices=[("30", "30" + " " + _("minutes")), ("60", _("Hour")), ("1440", _("Once per day"))])
 
