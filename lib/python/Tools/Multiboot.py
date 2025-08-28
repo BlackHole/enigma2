4f908d5bf636f2d02a774ee4c05789154b2fb7ab
dpeddi <1086815+dpeddi@users.noreply.github.com>
2025-08-28 20:06:32 +0000

    fix issue on dual after ubi multiboot introduction

--- lib/python/Tools/Multiboot.py
+++ lib/python/Tools/Multiboot.py
@@ -133,6 +133,8 @@ def getMultibootslots():
 					struct_fmt = "B"
 					flag = f.read(struct.calcsize(struct_fmt))
 					slot = struct.unpack(struct_fmt, flag)
+					SystemInfo["MultiBootSlot"] = int(slot[0])
+					print("[Multiboot][MultiBootSlot]1 current slot used:", SystemInfo["MultiBootSlot"])
 			elif bootArgs and SystemInfo["HasRootSubdir"] and "root=/dev/sda" not in bootArgs and not UBIMB:							# RootSubdir receiver or sf8008 receiver with root in eMMC slot
 				slot = [x[-1] for x in bootArgs.split() if x.startswith("rootsubdir")]
 				SystemInfo["MultiBootSlot"] = int(slot[0])
