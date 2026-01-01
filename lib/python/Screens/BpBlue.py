e52a6ae114378c05a33faae0854ffd4e41670674
Ev0-BH <Ev0_BhT@outlook.com>
2026-01-01 13:34:35 +0000

    [BpBlue] Poll ecm.info

--- lib/python/Screens/BpBlue.py
+++ lib/python/Screens/BpBlue.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from enigma import eTimer
+
 from Screens.Screen import Screen
 from Screens.MessageBox import MessageBox
 from Screens.About import About
@@ -82,6 +84,9 @@ class DeliteBluePanel(Screen):
 		self.emlist = []
 		self["list"] = MenuList(self.emlist)
 		self.onShow.append(self.updateBP0)
+		self.EcmInfoPollTimer = eTimer()
+		self.EcmInfoPollTimer.callback.append(self.setEcmInfo)
+		self.EcmInfoPollTimer.start(1000)
 
 	def updateBP0(self):
 		self.emlist = []
@@ -162,6 +167,18 @@ class DeliteBluePanel(Screen):
 						self.camnames[name] = "/usr/camscript/" + fil
 				f.close()
 
+	def setEcmInfo(self):
+		mytext = ""
+		if fileExists("/tmp/ecm.info"):
+			f = open("/tmp/ecm.info", 'r')
+			for line in f.readlines():
+				mytext = mytext + line.strip().replace("caid", "Caid").replace("pid", "Pid").replace("prov", "Prov").replace("chid", "Chid").replace("reader", "Reader").replace("from", "From").replace("protocol", "Protocol").replace("hops", "Hops").replace("ecm time", "Ecm Time").replace("cw", "CW ").replace("local", "Local").replace("using", "Using").replace("address", "Address").replace("system", "System").replace("source", "Source").replace("internal", "Internal") + "\n"
+			f.close()
+		if len(mytext) < 5:
+			mytext = "\n\n    " + _("Ecm Info Not Available.")
+
+		self["Ecmtext"].setText(mytext)
+
 	def updateBP(self):
 		try:
 			name = ServiceReference(self.session.nav.getCurrentlyPlayingServiceReference()).getServiceName()
@@ -216,17 +233,8 @@ class DeliteBluePanel(Screen):
 				break
 			pos += 1
 
-		mytext = ""
-		if fileExists("/tmp/ecm.info"):
-			f = open("/tmp/ecm.info", 'r')
-			for line in f.readlines():
-				mytext = mytext + line.strip().replace("caid", "Caid").replace("pid", "Pid").replace("prov", "Prov").replace("chid", "Chid").replace("reader", "Reader").replace("from", "From").replace("protocol", "Protocol").replace("hops", "Hops").replace("ecm time", "Ecm Time").replace("cw", "CW ").replace("local", "Local").replace("using", "Using").replace("address", "Address").replace("system", "System").replace("source", "Source").replace("internal", "Internal") + "\n"
-			f.close()
-		if len(mytext) < 5:
-			mytext = "\n\n    " + _("Ecm Info Not Available.")
-
 		self["activecam"].setText(self.defCamname)
-		self["Ecmtext"].setText(mytext)
+		self.setEcmInfo()
 
 	def getServiceInfoValue(self, what, myserviceinfo):
 		v = myserviceinfo.getInfo(what)
