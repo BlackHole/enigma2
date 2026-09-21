3005b8854fdad2fe6de5b05140488ef68b4790b8
Ev0-BH <Ev0_BhT@outlook.com>
2026-09-21 20:31:51 +0100

    Remove Passthrough Fix as Ultimo4K and Duo4KSE no longer require this workaround in Enigma2

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -343,7 +343,7 @@ def InitAVSwitch():
 		("ac3", _("Dolby Digital"))
 	], default="off")
 	if SystemInfo["Vu_EAC3_fix"]:
-		config.av.passthrough_fix = ConfigYesNo(default=True)
+		config.av.passthrough_fix = ConfigYesNo(default=False)
 	config.av.yuvenabled = ConfigYesNo(default=True)
 	colorformat_choices = {
 		"cvbs": _("CVBS"),
